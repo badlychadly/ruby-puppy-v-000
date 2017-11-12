48973929902a558f746b0b54c5194f12a81b5a96
Chad Montoya <chadmontoya21@yahoo.com>
2017-11-12 20:26:30 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,7 +6,7 @@ class Dog
 
   def initialize(name)
     @name = name
-    @@all << self
+    @@all << self.name
   end
 
   def self.all
@@ -22,3 +22,9 @@ class Dog
   end
 
 end
+
+
+
+# pluto = Dog.new("Pluto")
+# fido = Dog.new("Fido")
+# maddy = Dog.new("Maddy")
